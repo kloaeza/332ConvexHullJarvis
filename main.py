da048a8d7dcdc1f75dff200316e101825fc3a56a
kloaezaluna <kloaezaluna@mtech.edu>
2026-03-11 12:34:32 -0600

    Orientation helper function implemented

--- main.py
+++ main.py
@@ -2,6 +2,22 @@ from typing import List, Tuple
 
 Point = Tuple[float, float]
 
+# Helper function that determines the turn direction
+def get_orientation(p, q, r):
+    val = (q[0] - p[0]) * (r[1] - p[1]) - (q[1] - p[1]) * (r[0] - p[0])
+
+    # If values are collinear, return 0 
+    if val == 0:
+        return 0
+    
+    # If values positive, CCW, return 2
+    if val > 0:
+        return 2
+    
+    # Else values CW, return 1
+    else:
+        return 1
+
 def convex_hull_jarvis(points: List[Point]) -> List[Point]:
    pass
 
