d8a76de584127e38fad128f8671b56bccf1e2721
Kevin Loaeza <kloaezaluna@mtech.edu>
2026-03-14 15:08:23 -0600

    Find leftmost point implementation

--- main.py
+++ main.py
@@ -19,7 +19,36 @@ def get_orientation(p, q, r):
         return 1
 
 def convex_hull_jarvis(points: List[Point]) -> List[Point]:
-   pass
+   
+    n = len(points)
+
+    # Return if less than 3 points given
+    if n < 3:
+        return points
+    
+    # Finds leftmost point for starting anchor
+    leftmost = 0
+
+    for i in range(1, n):
+        # If x coordinate smaller, leftmost = i
+        if points[i][0] < points[leftmost][0]:
+            leftmost = i
+
+        # If same x coordinate, pick bottom most y coordinate
+        elif points[i][0] == points[leftmost][0]:
+            if points[i][1] < points[leftmost][1]:
+                leftmost = 1
+
+    hull = []
+    p = leftmost
+
+    # Gift Wrapping process
+    while True:
+        # Adds anchor point to hull
+        hull.append(points[p])
+
+        # Picks next point in the list as first q
+        q = (p + 1) % n
 
 
 if __name__ == "__main__":
