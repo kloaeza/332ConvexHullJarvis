53b40ed7757fb227efc31b02259a32cb40ef54ae
Kevin Loaeza <kloaezaluna@mtech.edu>
2026-03-15 16:16:39 -0600

    Tests modified in tests.py

--- tests.py
+++ tests.py
@@ -5,28 +5,36 @@ from main import convex_hull_jarvis
 class TestMathFunctions(unittest.TestCase):
     
     def test_example_from_prompt(self):
-        exit(1)
+        points = [(0, 3), (2, 2), (1, 1), (2, 1), (3, 0), (0, 0), (3, 3)]
+        self.assertEqual(convex_hull_jarvis(points), [(0, 0), (3, 0), (3, 3), (0, 3)])
 
     def test_square_with_inner_points(self):
-        exit(1)
+        points = [(0, 0), (5, 0), (5, 5), (0, 5), (3, 3), (1, 3)]
+        self.assertEqual(convex_hull_jarvis(points), [(0, 0), (5, 0), (5, 5), (0, 5)])
 
     def test_duplicates(self):
-        exit(1)
+        points = [(0, 0), (0, 0), (1, 1), (1, 1), (1, 0), (1, 0), (0, 1), (0, 1)]
+        self.assertEqual(convex_hull_jarvis(points), [(0, 0), (0, 0), (1, 0), (1, 0), (1, 1), (1, 1), (0, 1), (0, 1)])
 
     def test_all_collinear(self):
-        exit(1)
+        points = [(0, 0), (1, 1), (2, 2), (3, 3), (4, 4)]
+        self.assertEqual(convex_hull_jarvis(points), [(0, 0), (1, 1), (2, 2), (3, 3), (4, 4)])
 
     def test_two_points(self):
-        exit(1)
+        points = [(0, 0), (4, 4)]
+        self.assertEqual(convex_hull_jarvis(points), [(0, 0), (4, 4)])       
 
     def test_single_point(self):
-       exit(1)
+        points = [(0, 0)]
+        self.assertEqual(convex_hull_jarvis(points), [(0, 0)])
 
     def test_triangle(self):
-        exit(1)
+        points = [(0, 0), (0, 6), (3, 3)]
+        self.assertEqual(convex_hull_jarvis(points), [(0, 0), (3, 3), (0, 6)])   
 
     def test_collinear_on_edges(self):
-        exit(1)
+        points = [(0, 0), (0, 2), (2, 2), (2, 0), (1, 0)]
+        self.assertEqual(convex_hull_jarvis(points), [(0, 0), (2, 0), (2, 2), (0, 2)])
 
 if __name__ == "__main__":
     unittest.main()
